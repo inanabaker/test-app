bac1803c149a5d7292c53a239a5943ca683e87b6
bakina <inana.baker@kartverket.no>
2026-01-26 10:09:27 +0100

    feat: add secret handling and simple frontend pages

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello():
     return "Hello World hilser fra Argo!"
 
+def get_secret():
+    return os.getenv("InanaTopSecretKey", "SECRET NOT FOUND")
+
+@app.route("/")
+def index():
+    return render_template(
+        "index.html",
+    )
+
+@app.route("/reveal")
+def show_secret():
+    secret = get_secret()
+    return render_template(
+        "secret.html", secret=secret
+    )
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
