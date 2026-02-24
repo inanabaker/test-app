ebc1c51d6ce7ee8067ebbc7d8336c12358d9833a
bakina <inana.baker@kartverket.no>
2026-02-24 12:30:36 +0100

    fikser requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, render_template
-from databaseconnection import engine
+from flask import Flask, render_template, jsonify
+from sqlalchemy import text
 
+from databaseconnection import engine
 
 app = Flask(__name__)
 
@@ -33,6 +34,18 @@ def init_db():
             sql_script = f.read()
         conn.exec_driver_sql(sql_script)
     print("Database initialisert!")
+
+@app.route("/test-data")
+def get_test_data():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT id, text, ST_AsText(geom) FROM inanas_data;"))
+        rows = result.fetchall()
+
+        data = [
+            {"id": row[0], "text": row[1], "geom": row[2]}
+            for row in rows
+        ]
+    return jsonify(data)
 
 print("Init DB...")
 init_db()
--- init.sql
+++ init.sql
@@ -7,3 +7,9 @@ CREATE TABLE IF NOT EXISTS inanas_data (
 );
 
 CREATE INDEX IF NOT EXISTS idx_messages_geometry ON inanas_data USING gist (geom);
+
+INSERT INTO inanas_data (text, geom)
+VALUES (
+  'Test data',
+  ST_SetSRID(ST_GeomFromText('POINT(0 0)'), 25833)
+);
