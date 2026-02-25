7c82a0752b6e2ca9eb7a6fcc6540b7a198d65241
bakina <inana.baker@kartverket.no>
2026-02-25 08:57:33 +0100

    lagt til støtte til SSL-sertifikater

--- databaseconnection.py
+++ databaseconnection.py
@@ -3,4 +3,12 @@ import os
 
 DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
 
+if os.getenv("PGSSLCA"):
+    DATABASE_URL += (
+        f"?sslmode=require"
+        f"&sslrootcert={os.getenv('PGSSLCA')}"
+        f"&sslcert={os.getenv('PGSSLCERT')}"
+        f"&sslkey={os.getenv('PGSSLKEY')}"
+    )
+
 engine = create_engine(DATABASE_URL)
